717a42673c3abdd1882536433f97030f6cd02bc6
dennismacc <dennis.maccarthy@gmail.com>
2022-08-02 23:51:37 -0700

    install tailwind css

---
+++ my-react-app/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.js',
+    './src/**/*.jsx',
+    './src/**/*.ts',
+    './src/**/*.tsx',
+    './src/**/*.html',
+    './src/**/*.css',
+    './src/**/*.scss',
+    './src/**/*.json',
+    './src/**/*.md',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
