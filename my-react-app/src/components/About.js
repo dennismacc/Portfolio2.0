5dc01f4d3b95253ac133f8445d33aca881b3e39a
dennismacc <dennis.maccarthy@gmail.com>
2022-08-03 15:04:09 -0700

    work on projects page(add logic to display projects and info). add image to about.

--- my-react-app/src/components/About.js
+++ my-react-app/src/components/About.js
@@ -5,8 +5,8 @@ export default function About() {
     <section id="about">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi! I'm Dennis.
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-black">
+            Hi, my name is Dennis.
             <br className="hidden lg:inline-block" />
             I'm an aspiring Full-Stack Web Developer.
           </h1>
@@ -37,9 +37,9 @@ export default function About() {
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
           <img
-            className="object-cover object-center rounded"
+            className="object-cover object-center rounded shadow-lg w-8/12 h-10/12"
             alt="hero"
-            src="./coding.svg"
+            src={require("../assets/me.JPG")}
           />
         </div>
       </div>
---
+++ my-react-app/src/data.js
@@ -0,0 +1,21 @@
+import WhatStreamin from "./assets/projectmodal.gif";
+import Dudl from "./assets/dudl_homepage.gif";
+
+export const projects = [
+    {
+      title: "What's Streamin?'",
+      subtitle: "HTML CSS JS",
+      description:
+        "A simple web application that randomly generates a movie or show to watch based on user input criteria.",
+      image: WhatStreamin,
+      link: "https://github.com/dennismacc/Stream_Availability",
+    },
+    {
+      title: "DÜDL",
+      subtitle: "HTML CSS JS Express Socket.io Handlebars", 
+      description:
+        "DÜDL is a multiplayer drawing and guessing game that can be played with friends anytime, anywhere!",
+      image: Dudl,
+      link: "https://github.com/dennismacc/DUDL-Game",
+    },
+  ];
