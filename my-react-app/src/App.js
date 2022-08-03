fe6091013b5c86f220fc953154c817ee7d85135c
dennismacc <dennis.maccarthy@gmail.com>
2022-08-03 00:01:18 -0700

    fix files paths in App.js

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import About from "./About";
-import Contact from "./Contact";
-import Navbar from "./Navbar";
-import Projects from "./Projects";
-import Skills from "./Skills";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 export default function App() {
   return (
